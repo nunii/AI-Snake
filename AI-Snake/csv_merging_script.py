cee2e7d4fcb3aba52565a9ca8c0a74cbdca987e9
nunii <33713479+nunii@users.noreply.github.com>
2018-11-17 18:05:57 +0200

    Update csv_merging_script.py

--- AI-Snake/csv_merging_script.py
+++ AI-Snake/csv_merging_script.py
@@ -8,7 +8,7 @@ for files in glob.glob("*.csv"):
 f_out = open("Final_data_set.csv", "a")
 for file in glob.glob("*.csv"):
     f = open(file)
-    #f.next()  # skip the header
+    f.next()  # skip the header
     for line in f:
         f_out.write(line)
     f.close()
